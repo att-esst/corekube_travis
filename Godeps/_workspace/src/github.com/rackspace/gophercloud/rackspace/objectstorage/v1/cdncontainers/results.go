ca37b5b976ba0dd1d48355d6285d764847632a1a
Mike Metral <metral@gmail.com>
2015-01-16 10:36:22 -0800

    Remove godeps

--- Godeps/_workspace/src/github.com/rackspace/gophercloud/rackspace/objectstorage/v1/cdncontainers/results.go
+++
@@ -1,8 +0,0 @@
-package cdncontainers
-
-import "github.com/rackspace/gophercloud"
-
-// EnableResult represents the result of a get operation.
-type EnableResult struct {
-	gophercloud.HeaderResult
-}
--- Godeps/_workspace/src/github.com/rackspace/gophercloud/rackspace/objectstorage/v1/cdncontainers/urls.go
+++
@@ -1,7 +0,0 @@
-package cdncontainers
-
-import "github.com/rackspace/gophercloud"
-
-func enableURL(c *gophercloud.ServiceClient, containerName string) string {
-	return c.ServiceURL(containerName)
-}
