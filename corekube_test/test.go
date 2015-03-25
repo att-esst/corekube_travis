392d3d14043e527184b475f893bc3f621f2094c2
Mike Metral <metral@gmail.com>
2015-03-25 15:48:48 -0700

    Increate test timeout

--- corekube_test/test.go
+++ corekube_test/test.go
@@ -76,7 +76,7 @@ func overlayNetworksCountTest(
 }
 
 func runTests(config *util.HeatConfig, details *util.StackDetails) {
-	corekube_travis.StartTestTimeout(1, config, details, overlayNetworksCountTest)
+	corekube_travis.StartTestTimeout(5, config, details, overlayNetworksCountTest)
 }
 
 func main() {
