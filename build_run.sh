54375113f36729a200a7b0293cd59d2f38bea1e1
Mike Metral <metral@gmail.com>
2015-04-30 16:18:03 -0700

    Init docker

---
+++ build_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RUN_TEST=$1
+ENV_FILE=$2
+TEMPLATE=$3
+
+docker rm -f $RUN_TEST
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+result=`docker build --rm -t $RUN_TEST -f $DIR/$RUN_TEST/Dockerfile .`
+echo "$result"
+
+echo ""
+echo "=========================================================="
+echo ""
+
+build_status=`echo $result | grep "Successfully built"`
+
+if [ "$build_status" ] ; then
+    docker run --name $RUN_TEST -d -v $ENV_FILE:/tmp/env -v $TEMPLATE:/tmp/template.yaml $RUN_TEST
+fi
