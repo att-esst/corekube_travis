b36d401a7ae13df723a93f934c33bdd62a08062b
Mike Metral <metral@gmail.com>
2015-06-15 15:53:49 -0700

    Remove docker

--- infra_test/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/gopath/bin/infra_test \
-    --authUrl=$TRAVIS_OS_AUTH_URL \
-    --keypair=$TRAVIS_OS_KEYPAIR \
-    --password=$TRAVIS_OS_PASSWORD \
-    --username=$TRAVIS_OS_USERNAME \
-    --tenantId=$TRAVIS_OS_TENANT_ID \
-    --templateFile="/tmp/corekube-cloudservers.yaml"
