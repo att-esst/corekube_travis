8fd87562dccf8f682a1b8ddcfe0618e74eb9e529
Mike Metral <metral@gmail.com>
2015-06-15 13:33:23 -0700

    Breakup

--- build_run.sh
+++ build_run.sh
@@ -17,7 +17,8 @@ if [ "$build_status" ] ; then
     pushd $GOPATH/src/github.com/metral/overlord;
     COMMIT=`git rev-parse --short HEAD`
     echo $COMMIT;
-    sed -i "s#TRAVIS_COMMIT.*#TRAVIS_COMMIT=$COMMIT#g" /tmp/travis ; popd
+    sed -i "s#TRAVIS_COMMIT.*#TRAVIS_COMMIT=$COMMIT#g" /tmp/travis
+    popd
 
     docker run --env-file=/tmp/travis -i --entrypoint /bin/bash -t --rm -v ~/go/src/github.com/metral/goheat:/gopath/src/github.com/metral/goheat -v ~/go/src/github.com/metral/goutils:/gopath/src/github.com/metral/goutils -v ~/go/src/github.com/metral/corekube_travis:/gopath/src/github.com/metral/corekube_travis overlord_test -s
 fi
