d4b73bd700720fa64aa3c08e97db9fc50d6ea94e
Mike Metral <metral@gmail.com>
2015-06-15 15:14:54 -0700

    Fix dockerness & template issues

--- build_run.sh
+++ build_run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 RUN_TEST=$1
-ENV_FILE=$2
-TEMPLATE=$3
+TEMPLATE=$2
 
 docker rm -f $RUN_TEST
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -17,9 +16,17 @@ echo ""
 build_status=`echo $result | grep "Successfully built"`
 
 if [ "$build_status" ] ; then
+    cp ~/travis /tmp/travis
+    # til popd - for overlord_test - not used in infra_test
+    pushd $GOPATH/src/github.com/metral/overlord;
+    COMMIT=`git rev-parse --short HEAD`
+    echo $COMMIT;
+    sed -i "s#TRAVIS_COMMIT.*#TRAVIS_COMMIT=$COMMIT#g" /tmp/travis
+    popd
+
     if [ "$TEMPLATE" ] ; then
-        docker run --name $RUN_TEST -d -v $ENV_FILE:/tmp/env -v $TEMPLATE:/tmp/corekube-cloudservers.yaml $RUN_TEST
+        docker run --name $RUN_TEST -d --env-file=/tmp/travis -v $TEMPLATE:/tmp/corekube-cloudservers.yaml $RUN_TEST
     else
-        docker run --name $RUN_TEST -d -v $ENV_FILE:/tmp/env $RUN_TEST
+        docker run --name $RUN_TEST -d --env-file=/tmp/travis $RUN_TEST
     fi
 fi
